be7d35df6fb7533a411900c4e1bc50a85d94c0df
Wasim Ul Masood <masoodwasim@gmail.com>
2022-08-23 06:51:02 +0000

    adding staging models

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,32 @@
+/*
+select *,
+ordersellingprice - ordercostprice as orderprofit
+from {{ ref('raw_orders') }}
+
+*/
+
+/* Execute particular model with dependencies run command - dbt run -s +stg_orders   */
+
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerid=c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid=p.productid
+
+
+
+
